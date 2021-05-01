c9f707478d857608ae8ccb33b15dd902aba43d08
JeanFerme <martin.cressein@uha.fr>
2021-05-01 17:13:21 +0200

    QT_Project
    Transformation towards a QT project

--- fuzzy/main.cpp
+++ fuzzy/main.cpp
@@ -1,7 +1,12 @@
+#include "mainwindow.h"
+
 #include "./tests/tests.h"
+#include <QApplication>
 
-
-int main() {
-	tests();
-	return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
